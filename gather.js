68287eeb709017ae99eddd019afae3e1ca3d2a1f
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-01-06 01:52:47 -0500

    working on get request for instagram

--- gather.js
+++ gather.js
@@ -1,3 +1,7 @@
+var csv = require("ya-csv");
+var http = require("http");
+
+
 var config = require("./config.js");
 
 var washington = {};
@@ -5,14 +9,28 @@ washington.csv = "./data/washington/data.csv";
 washington.images = "./data/washington/images/";
 washington.locations = [];
 washington.locations[0] = {};
-washington.locations[0].lat = "";
-washington.locations[0].long = "";
-washington.locations[0].distance = "";
+washington.locations[0].lat = "38.895111";
+washington.locations[0].long = "-77.036667";
+washington.locations[0].distance = "5000"; //meters
 
-var csv = require("ya-csv");
+console.log(washington);
 
 var w = csv.createCsvFileWriter(washington.csv, {'flags': 'a'});
+//w.writeRecord(["test"]);
 
-console.log(config);
+var curlData = {};
+curlData.host = "api.instagram.com";
+curlData.port = 443;
+curlData.path = "/v1/media/search?client_id="+config.instagram_id+"&lat="+washington.locations[0].lat+"&lng="+washington.locations[0].long+"&distance="+washington.locations[0].distance;
+curlData.method = "GET";
 
-w.writeRecord(["test"]);
+console.log(curlData);
+
+var req = http.request(curlData)
+.on('error', function(err){
+	console.log(err);
+})
+.on('end', function(res){
+	console.log(res);
+});
+
---
+++ notes/city data.md
@@ -0,0 +1,6 @@
+# Washington, DC
+
+Lat: 38.895111
+Long: -77.036667
+Area: 177.0 km
+Population: 632,323
